d4259d34b4e3873d9a04d91129bade9e2d097093
sirwilliamiv <wjconnolly2@gmail.com>
2016-11-30 15:44:54 -0600

    deleted notes and random text

--- converter.js
+++ converter.js
@@ -1,32 +1,6 @@
 
 
-// Write a program that will convert a temperature from fahrenheit to celsius, or from celsius to fahrenheit.
 
-
-
-
-// If the temperature is greater than 90F/32C the color of the converted temperature should be red.
-// If the temperature is less than 32F/0C the color of the converted temperature should be blue.
-// For any other temperature, the color should be green.
-
-
-
-
-
-// / Add an event handler to the input field that checks if the user pressed the enter key, and if that happens, perform the conversion.
-// enterkey+addEventListener("")
-
-
-
-
-// put a change event on celsius/fahrenheit radio buttons
-// 
-//document.querySelector(buttonthing).addeventleister('look up button fuctnionsmdn', function () {
-//grabs input
-// looks at radio ID
-// if celcuius true run celc
-// else run farenheign
-// }\\\
 
 
 
@@ -130,31 +104,3 @@ document.querySelector('#clearText').addEventListener('click', function() {
 )
 
 
-// -------------------------Color based on Temp----------------------
-// If the temperature is greater than 90F/32C the color of the converted temperature should be red.
-// If the temperature is less than 32F/0C the color of the converted temperature should be blue.
-// For any other temperature, the color should be green.
-
-// red 
-
-
-
-
-
-
-
-
-// Get a reference to the button element in the DOM
-// var button = document.getElementById("converter");
-
-// This function should determine which conversion should
-// // happen based on which radio button is selected.
-// function determineConverter (clickEvent) {
-//   console.log("event", clickEvent);
-// }
-
-
-// toCelsius(36)
-
-// Assign a function to be executed when the button is clicked
-// button.addEventListener("click", determineConverter);
